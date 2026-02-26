a8d5edb613ca2f0308868cc51f9c2736843af081
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2026-02-26 12:03:20 +0530

    Know about how to define class and Multiple line jsx

--- App.js
+++ App.js
@@ -1,22 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => ReactElement-JS Object => HTMLElement(render)
-
-const heading = React.createElement(
-      'h1',
-      {id:"heading"},
-      "Namaste React🚀"
-);
-console.log(heading);
-
-// JSX - HTML-like or XML-like syntax 
+// JSX - HTML-like or XML-like syntax
 // JSX (transpiled before it reaches the js) - Parcel - babel
-// JSX => React.createElement => ReactElement-JS Object => HTMLElement(render)
+// JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
 
-const jsxHeading = <h1 id="heading">Namaste React🚀 using JSX</h1>;
+const jsxHeading = (
+    <h1 className="head" tabIndex="1">
+      Namaste React🚀 using JSX
+    </h1>
+);
+
 console.log(jsxHeading);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(jsxHeading);
