bdae4d01b8f7d4312f6bd33d0214885d6636e852
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2026-02-26 12:44:00 +0530

    React Function components

--- App.js
+++ App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// JSX - HTML-like or XML-like syntax
-// JSX (transpiled before it reaches the js) - Parcel - babel
-// JSX => Babel transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
-
-const jsxHeading = (
+// React Element
+const heading = (
     <h1 className="head" tabIndex="1">
       Namaste React🚀 using JSX
     </h1>
 );
 
-console.log(jsxHeading);
+// React component
+// React Function Component - NEW
+
+const Heading = () => {
+      return <h1>Namaste React frm React Function Component</h1>
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(heading);
