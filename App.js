7e672dfb7d9337ebe81ce3bb174418f1a2c10f7e
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2026-02-26 13:44:18 +0530

    Multiple way adding React component inside React component

--- App.js
+++ App.js
@@ -1,19 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const elem = <span>React Element</span>
-
-const title = (
+const Title = () => (
   <h1 className="head" tabIndex="1">
-    {elem}
     Namaste React🚀 using JSX
   </h1>
 );
 
 const HeadingComponent = () => (
   <div id="container">
-      {title}
-
+      {Title()}
+      <Title />
+      <Title></Title>
     <h1 className="heading">Namaste React frm React Function Component</h1>
   </div>
 );
