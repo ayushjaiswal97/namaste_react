a8918a6929e1c38cd50933c80d3a3e0d8bd40d4b
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2026-02-27 14:23:47 +0530

    Passing props to resName and cusine

--- App.js
+++ App.js
@@ -37,7 +37,7 @@ const Header = () => {
   );
 };
 
-const RestaurantCard = () => {
+const RestaurantCard = (props) => {
   return (
     <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
       <img 
@@ -45,8 +45,8 @@ const RestaurantCard = () => {
        alt="res-logo" 
        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2026/2/12/3fdcae9a-9614-4251-bf52-7cfe6bdeb0fa_101352.JPG" 
       />
-      <h3>Pizza Hut</h3>
-      <h4>Italian, Pizza</h4>
+      <h3>{props.resName}</h3>
+      <h4>{props.cusine}</h4>
       <h4>4.3 stars</h4>
       <h4>35-40 mins</h4>
     </div>
@@ -59,22 +59,14 @@ const Body = () => {
     <div className="body">
       <div className="search">Search</div>
       <div className="res-container">
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
-        <RestaurantCard />
+        <RestaurantCard 
+        resName="Pizza Hut"
+        cusine="Italian, Pizza"
+        />
+        <RestaurantCard 
+        resName="Burger King"
+        cusine="American, Burger" 
+        />
       </div>
     </div>
   )
