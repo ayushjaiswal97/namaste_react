6b4256c345d0bcfa50764b89ffb08a25d92a0e8a
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2026-02-27 17:15:48 +0530

    Passing props through Destructuring objects

--- App.js
+++ App.js
@@ -37,7 +37,7 @@ const Header = () => {
   );
 };
 
-const RestaurantCard = (props) => {
+const RestaurantCard = ({resName, cusine}) => {
   return (
     <div className="res-card" style={{backgroundColor: "#f0f0f0"}}>
       <img 
@@ -45,8 +45,8 @@ const RestaurantCard = (props) => {
        alt="res-logo" 
        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2026/2/12/3fdcae9a-9614-4251-bf52-7cfe6bdeb0fa_101352.JPG" 
       />
-      <h3>{props.resName}</h3>
-      <h4>{props.cusine}</h4>
+      <h3>{resName}</h3>
+      <h4>{cusine}</h4>
       <h4>4.3 stars</h4>
       <h4>35-40 mins</h4>
     </div>
