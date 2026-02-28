f7e127ab8077e3cf1697672c99b41c1ce66a96f1
ayushjaiswal97 <ayushjaiswal9099@gmail.com>
2026-02-28 14:17:14 +0530

    Create state variable and know about hooks, React algorithm(reconiliation algo(React Fiber) Virtual DOM Or Diff algo)

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,79 +1,11 @@
 import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 
 const Body = () => {
-  // Normal JS Variable
-  let listsOfRestaurants = [
-    {
-      info:{
-      id: "101352",
-      name: "Pizza Hut",
-      cloudinaryImageId:
-        "RX_THUMBNAIL/IMAGES/VENDOR/2026/2/12/3fdcae9a-9614-4251-bf52-7cfe6bdeb0fa_101352.JPG",
-      locality: "Bokaro Steel City",
-      areaName: "Bokaro Mall",
-      costForTwo: "₹350 for two",
-      cuisines: ["Pizza"],
-      avgRating: 3.6,
-      parentId: "721",
-      avgRatingString: "4.3",
-      totalRatingsString: "5.1K+",
-      sla: {
-        deliveryTime: 43,
-        lastMileTravel: 5.8,
-        serviceability: "SERVICEABLE",
-        slaString: "38-43 mins",
-        lastMileTravelString: "5.8 km",
-        iconType: "ICON_TYPE_EMPTY",
-      }},
-    },
-    {
-      info:{
-      id: "101353",
-      name: "MCD",
-      cloudinaryImageId:
-        "RX_THUMBNAIL/IMAGES/VENDOR/2026/2/12/3fdcae9a-9614-4251-bf52-7cfe6bdeb0fa_101352.JPG",
-      locality: "Bokaro Steel City",
-      areaName: "Bokaro Mall",
-      costForTwo: "₹350 for two",
-      cuisines: ["Pizza"],
-      avgRating: 4.3,
-      parentId: "721",
-      avgRatingString: "4.3",
-      totalRatingsString: "5.1K+",
-      sla: {
-        deliveryTime: 43,
-        lastMileTravel: 5.8,
-        serviceability: "SERVICEABLE",
-        slaString: "38-43 mins",
-        lastMileTravelString: "5.8 km",
-        iconType: "ICON_TYPE_EMPTY",
-      }},
-    },
-    {
-      info:{
-      id: "101354",
-      name: "Burger King",
-      cloudinaryImageId:
-        "RX_THUMBNAIL/IMAGES/VENDOR/2026/2/12/3fdcae9a-9614-4251-bf52-7cfe6bdeb0fa_101352.JPG",
-      locality: "Bokaro Steel City",
-      areaName: "Bokaro Mall",
-      costForTwo: "₹350 for two",
-      cuisines: ["Pizza"],
-      avgRating: 4.3,
-      parentId: "721",
-      avgRatingString: "4.3",
-      totalRatingsString: "5.1K+",
-      sla: {
-        deliveryTime: 43,
-        lastMileTravel: 5.8,
-        serviceability: "SERVICEABLE",
-        slaString: "38-43 mins",
-        lastMileTravelString: "5.8 km",
-        iconType: "ICON_TYPE_EMPTY",
-      }},
-    },
-  ];
+   
+   // Local State variable => Super powerful variable
+   let [listsOfRestaurants, setListsOfRestaurants] = useState(resList);
 
   return (
     <div className="body">
@@ -81,10 +13,11 @@ const Body = () => {
         <button
         className="filter-btn"
           onClick={() => {
-            listsOfRestaurants = listsOfRestaurants.filter(
-             (res) => res.info.avgRating > 4.0
+            const filteredList = listsOfRestaurants.filter(
+             (res) => res.info.avgRating > 4.5
             );
-            console.log(listsOfRestaurants);
+            console.log(filteredList);
+            setListsOfRestaurants(filteredList);
           }}
           >Top Rated Restaurants
         </button>
